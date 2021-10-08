a74fef44cddb44d46682ff45eccc5698f5652505
Dorcas-BD <52034693+Dorcas-BD@users.noreply.github.com>
2021-10-08 07:46:07 +0100

    Update

--- e-cycle/Pages/Recycle/script.js
+++ e-cycle/Pages/Recycle/script.js
@@ -86,48 +86,6 @@ defaultBtn.addEventListener("change", function(){
 })
 
 
-
-
-
-
-
-
-
-
-
-// const video = document.getElementById('video');
-// const canvas = document.getElementById('canvas');
-// const snap = document.getElementById('snap');
-// const errorMsElement = document.getElementById('span#ErrorMsg');
-
-// const constraints = {
-//     audio : true;
-//     video: {
-//         width: 1280, 
-//         height: 720
-//     }
-// };
-
-// async function init() {
-//     try{
-//         const stream = await navigator.mediaDevices.getMedia(constraints);
-//         handleSuccess(stream);
-//     }
-//     catch(e) {
-//         errorMsElement.innerHTML = `naviator.getUserMedia.error:${e.toString()}`;
-
-//     }
-// }
-
-
-
-// function handleSuccess(stream) {
-//     window.stream = stream;
-//     video.srcObject = stream
-// }
-
-// init();
-
 // var context = canvas.getContext('2d');
 snap.addEventListener("click", function() {
     context.drawImage(video, 0, 0, 640, 480);
